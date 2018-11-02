c1e90842172b511ddc8ad9a55a6e872db2309adb
yusulian <yusulian@yy.com>
2018-11-02 14:19:02 +0800

    检查子组或项目是否存在

--- python/python-gitlab/lab_create_git_repo.py
+++ python/python-gitlab/lab_create_git_repo.py
@@ -19,27 +19,43 @@ def create_project_repo(ini_file):
     fp.close()
 
 
+    #repo_url = 'https://git.yy.com/gittooltest/g-ci-01/third-project.git'
+    repo_url = 'https://git.yy.com/apps/kaixindou/kxd-ios.git'
+    parent_group_name = repo_url.split('/')[3]
+    sub_group_name  = repo_url.split('/')[4]
+    project_name = repo_url.split('/')[5].replace('.git','')
+    print (parent_group_name,sub_group_name,project_name)
+
     ## login
     gl = gitlab.Gitlab.from_config('somewhere', ['/Users/elian/.python-gitlab.cfg'])
 
-    group = gl.groups.get('gittooltest')  # 获取parent组对象
+    group_all = gl.groups.list(search=parent_group_name) # list会列出重名的组
+    print (group_all)
+    namespaces = gl.namespaces.list(search=parent_group_name) # 搜索指定组名获取namespaces
+    print (namespaces)
+
+    group = gl.groups.get(parent_group_name)  # 获取parent组对象
     print (group)
     subgroup_id_list = group.subgroups.list() # 获取所有子组group id
     print (subgroup_id_list)
-    subgroup_id = group.subgroups.list(search="g-ci")[0].id # 指定要获取子组的group id
+    subgroup_id = group.subgroups.list(search=sub_group_name) # 指定要获取子组的group id
     print (subgroup_id)
-    subgroup = gl.groups.get(subgroup_id) # 根据id获取子组对象
-    projects = subgroup.projects.list()  # 获取子组下的项目
-    print (projects)
-    project = subgroup.projects.list(search="third-project")
-    print (project)
-    if project:
-        print ("project found")
+    if subgroup_id:
+        print ("%s subgroup found" % sub_group_name)
+        subgroup_id_value = subgroup_id[0].id
+        print (subgroup_id_value)
+        subgroup = gl.groups.get(subgroup_id_value) # 根据id获取子组对象
+        projects = subgroup.projects.list()  # 获取子组下的项目
+        print (projects)
+        project = subgroup.projects.list(search=project_name)
+        print (project)
+        if project:
+            print ("%s project found" % project_name)
+        else:
+            print ("%s project not exist" % project_name)
     else:
-        print ("project not found")
+        print ("%s subgroup not exist" % sub_group_name)
 
-    namespaces = gl.namespaces.list(search='gittooltest')
-    print (namespaces)
 
 if __name__ == '__main__':
     main()
